9b1c0ff298b8dcca073f0e534ea782141591a822
Shashy Dass <ShashyD@gmail.com>
2023-04-25 09:34:57 -0400

    Changes for build & deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
   return {
+    build: { target: 'esnext' },
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
